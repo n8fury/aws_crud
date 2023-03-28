235a10d96011d261c453bc11c3fa73b8f8d13c01
n8fury <mhjoy547@yahoo.com>
2023-03-29 02:00:35 +0600

    added routes and reponse

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,22 @@ import logging
 from decimal import Decimal
 
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodbTableName = 'demo_crud'
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(dynamodbTableName)
+
+getMethod = 'GET'
+postMethod = 'POST'
+deleteMethod = 'DELETE'
+patchMethod = 'PATCH'
+healthPath = '/health'
+productPath = '/product'
+productsPath = '/products'
+
+
 class CustomEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
@@ -23,4 +39,29 @@ def buildResponse(statusCode, body=None):
 
     if body is not None:
         response['body'] = json.dumps(body, cls=CustomEncoder)
+    return response
+
+
+def lambda_handler(event, context):
+    logger.info(event)
+    httpMethod = event['httpMethod']
+    path = event['path']
+    if httpMethod == getMethod and path == healthPath:
+        response = buildResponse(200)
+    elif httpMethod == getMethod and path == productPath:
+        response = getProduct(event['queryStringParameters']['productId'])
+    elif httpMethod == getMethod and path == productsPath:
+        response = getProducts()
+    elif httpMethod == postMethod and path == productPath:
+        response = saveProduct(json.loads(event['body']))
+    elif httpMethod == patchMethod and path == productPath:
+        requestBody = json.loads(event['body'])
+        response = modifyProduct(
+            requestBody['productId'], requestBody['updateKey'], requestBody['updateValue'])
+    elif httpMethod == deleteMethod and path == productPath:
+        requestBody = json.loads(event['body'])
+        response = deleteProduct(requestBody['productId'])
+    else:
+        response = buildResponse(404, 'Not Found')
+
     return response
