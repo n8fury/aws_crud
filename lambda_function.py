3ba6428ce6496a838c0b8f1e17889473c526506b
n8fury <mhjoy547@yahoo.com>
2023-03-29 01:58:53 +0600

    added custom_encoder for decimal values

---
+++ lambda_function.py
@@ -0,0 +1,14 @@
+import boto3
+import json
+import logging
+from decimal import Decimal
+
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+
+        return json.JSONEncoder.default(self, obj)
+
+
